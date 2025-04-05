746e6de758e7d8dcedec835feca7258b75bb54fd
Sejal Davla <77356703+sejaldavla@users.noreply.github.com>
2025-04-04 23:51:18 -0400

    Update slope_02.R

--- 2025/02 - Slope/slope_02.R
+++ 2025/02 - Slope/slope_02.R
@@ -1,1 +1,48 @@
 
+# load packages
+
+library(rvest)
+library(tidyverse)
+library(janitor)
+
+# data import
+
+ovechkin <- read_html("https://www.espn.com/nhl/player/stats/_/id/3101/alex-ovechkin")
+
+tables <- ovechkin |> 
+  html_table(fill = TRUE)
+
+df_o <- tables[[1]] |>
+  bind_cols(tables[[2]]) |>
+  filter(season != "Career") |>
+  mutate(player = rep("Ovechkin", 20),
+         season = row_number())
+
+gretzky <- read_html("https://www.espn.com/nhl/player/stats/_/id/4128/wayne-gretzky")
+
+tables <- gretzky |> 
+  html_table(fill = TRUE)
+
+df_g <- tables[[1]] |>
+  bind_cols(tables[[2]]) |>
+  filter(season != "Career") |>
+  mutate(player = rep("Gretzsky", 21),
+         season = row_number())
+
+
+df <- df_o |>
+  bind_rows(df_g) |>
+  clean_names() |>
+  select(c(season, player, gp, g, a, pts)) |>
+  rename(games = gp,
+         goals = g,
+         assists = a,
+         points = pts)
+
+
+df |>
+  ggplot(aes(x = season, y = goals, shape = player, color = player)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  theme_void()
+  
