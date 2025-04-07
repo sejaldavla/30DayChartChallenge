d39107dc3431d25b933aba68976f7da8990cfb76
Sejal Davla <sejaldavla@gmail.com>
2025-04-07 16:14:00 -0400

    main

--- 2025/02 - Slope/slope_02.R
+++ 2025/02 - Slope/slope_02.R
@@ -4,6 +4,11 @@
 library(rvest)
 library(tidyverse)
 library(janitor)
+library(ggimage)
+library(ggtext)
+library(sysfonts)
+library(showtext)
+library(grid)
 
 # data import
 
@@ -26,7 +31,7 @@ tables <- gretzky |>
 df_g <- tables[[1]] |>
   bind_cols(tables[[2]]) |>
   filter(season != "Career") |>
-  mutate(player = rep("Gretzsky", 21),
+  mutate(player = rep("Gretzky", 21),
          season = row_number())
 
 
@@ -37,12 +42,63 @@ df <- df_o |>
   rename(games = gp,
          goals = g,
          assists = a,
-         points = pts)
+         points = pts) |>
+  mutate(image = "hockey.png")
 
 
-df |>
-  ggplot(aes(x = season, y = goals, shape = player, color = player)) +
-  geom_point() +
-  geom_smooth(method = "lm", se = FALSE) +
-  theme_void()
+# fonts
+
+font_add_google("Bungee Tint", db_cache = FALSE)
+font_add_google("Inter", db_cache = FALSE)
+showtext_auto()
+
+# colors
+
+col_pal <- c("Gretzky" = "#FF4C00",
+             "Ovechkin" = "#041E42")
+
+# text
+
+st_text <- "<span style='color:#041E42'>**Alex Ovechkin**</span>'s goal scoring pace remained consistent with each advancing season <br>compared to <span style='color:#FF4C00'>**Wayne Gretzky**</span> whose performance declined in the later half of his career."
   
+caption <- "Source: NHL Stats • Graphic: Sejal Davla, PhD"
+
+
+slope_02 <- df |>
+  mutate(player = factor(player, levels = c("Ovechkin","Gretzky"))) |>
+  ggplot(aes(x = season, y = goals, color = player)) +
+  geom_image(aes(image = image), size = 0.02, asp = 6/4, alpha = 0.8) +
+  geom_smooth(method = "lm", se = FALSE) +
+  scale_color_manual(values = col_pal) +
+  scale_y_continuous(limits = c(0, 100),
+                     breaks = seq(0,100,20)) +
+  facet_wrap(~player) +
+  labs(title = "The Road to 894: Season Performance",
+       subtitle = st_text,
+       caption = caption) +
+  theme_minimal(base_family = "Inter") +
+  theme(legend.position = "none",
+        plot.title = element_markdown(family = "Bungee Tint",
+                                      size = 24,
+                                      hjust = 0.5,
+                                      margin = margin(1,0,0.5,0,"cm")),
+        plot.subtitle = element_textbox_simple(size = 18,
+                                               hjust = 0.5,
+                                               lineheight = 0.5,
+                                               margin = margin(0,0,0.5,0,"cm")),
+        plot.caption = element_markdown(size = 12,
+                                        hjust = 0.5),
+        axis.line = element_line(arrow = grid::arrow(length = unit(0.3, "cm"), 
+                                                       ends = "last"),
+                                 color = "grey50"),
+        panel.grid.minor = element_blank(),
+        strip.text = element_markdown(family = "Bungee Tint",
+                                      size = 12),
+        panel.spacing.x = unit(3,"lines"))
+
+ggsave("slope_02.png", width = 6, height = 4, dpi = 300, bg="white")
+
+
+
+
+
--- 2025/05 - Ranking/ranking_05.R
+++ 2025/05 - Ranking/ranking_05.R
@@ -5,6 +5,13 @@ library(rvest)
 library(tidyverse)
 library(janitor)
 library(ggbump)
+library(ggtext)
+library(glue)
+library(sysfonts)
+library(showtext)
+
+
+# data
 
 biotech <- read_html("https://en.wikipedia.org/wiki/List_of_largest_biomedical_companies_by_revenue")
 
@@ -29,22 +36,93 @@ df_rank <- df_clean |>
          revenue = round(revenue),
          country = str_sub(country, 1,3)) |>
   na.omit() |>
+  filter(year %in% c("2019","2020","2021","2022","2023","2024")) |>
   group_by(year) |>
   arrange(year, desc(revenue)) |>
   mutate(rank = row_number(desc(revenue))) |>
   filter(rank <= 10) |>
-  mutate(rank = rank *(-1)) |>
-  distinct()
+  distinct() |>
+  mutate(color = case_when(company == "Pfizer" ~ '#000485',
+                           .default = "#E7E7E7"))
 
-df_rank |>
-  ggplot(aes(factor(year), rank)) +
-  geom_point(aes(size = revenue, color = country)) +
-  geom_bump(aes(color = company)) 
+points_data <- df_rank |>
+  filter(company == "Pfizer")
+
+label_2019 <- df_rank |>
+  filter(year == 2019)
+
+label_2024 <- df_rank |>
+  filter(year == 2024)
 
 
+# fonts
+font_add_google("Noto Sans")
+showtext_auto()
+
+# text
+
+title_text <- "COVID-19 Pandemic Made Pfizer the Largest Pharma Company in Revenue"
+
+subtitle_text <- "This chart highlights how responding to the pandemic made <span style='color:#000485'>**Pfizer**</span> the top-ranking biomedical company by <span style='color:#000485'>revenue (in billion dollars)</span> in 2022. The 2020 COVID-19 pandemic doubled Pfizer’s earnings in just one year. Within two years, the company recorded $100 billion in revenue, 57% of which came from the <strong><em>COVID-19 vaccine and antiviral Paxlovid</em></strong>."
+
+caption <- "Source: Wikipedia • Graphic: Sejal Davla, PhD"
+
+# plot
+
+label_padding = 0.2
+
+ranking_05 <- df_rank |>
+  ggplot(aes(year, rank)) +
+  geom_bump(aes(group = company, color = I(color)),
+            linewidth = 1.2) +
+  geom_point(data = points_data,
+             aes(size = revenue, color = I(color)),
+             shape = 19) +
+  geom_text(data = points_data,
+            aes(label = revenue),
+            color = "white",
+            size = 10) +
+  geom_richtext(data = label_2019,
+                aes(x = year - label_padding, 
+                    y = rank, 
+                    label = glue("{company}"," ","**{rank}**")),
+                hjust = 1,
+                size = 10,
+                label.color = NA) +
+  geom_richtext(data = label_2024,
+                aes(x = year + label_padding, 
+                    y = rank, 
+                    label = glue("**{rank}**", "   ","{company}")),
+                hjust = 0,
+                size = 10,
+                label.color = NA) +
+  scale_x_continuous(limits=c(2018,2025), breaks=2019:2024) +
+  scale_y_reverse() +
+  scale_size(range = c(5, 10)) +
+  labs(x = "",
+       y = "",
+       size = "",
+       title = title_text,
+       subtitle = subtitle_text,
+       caption = caption) +
+  theme_minimal(base_family = "Noto Sans") +
+  theme(legend.position = "none",
+        axis.text.y = element_blank(),
+        panel.grid = element_blank(),
+        axis.text.x = element_text(size = 28),
+        plot.title = element_textbox_simple(color = '#000485',
+                                      size = 60, 
+                                      hjust = 0.5,
+                                      margin = margin(0.5,0,1,0,"cm")),
+        plot.subtitle = element_textbox_simple(size = 40,
+                                               hjust = 0.5,
+                                               lineheight = 0.5,
+                                               margin = margin(0,0,0.5,0,"cm")),
+        plot.caption = element_text(size = 24,
+                                    hjust = 0.5))
 
 
-
+ggsave("ranking_05.png", width = 10, height = 8, dpi = 300, bg = "white")
 
   
   
